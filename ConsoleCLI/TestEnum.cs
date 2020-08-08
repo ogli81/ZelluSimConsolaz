76869da5d5557133701ac384ea5be82670db6fe2
ogli81 <marcus@sternenschwarm.de>
2020-08-08 11:19:27 +0200

    Fixed minor issue in the TestEnum.

--- ConsoleCLI/TestEnum.cs
+++ ConsoleCLI/TestEnum.cs
@@ -6,17 +6,21 @@ using System.Threading.Tasks;
 
 namespace ZelluSimConsolaz.ConsoleCLI
 {
+    /// <summary>
+    /// This dummy class is only there to test flags-enums (bitfield-enums).
+    /// </summary>
     [Flags]
-    public enum TestEnum
+    public enum TestEnum : ushort //ushort gives us 16 bits (16 possible flags)
     {
-        FirstBlood,
-        SecondMinute,
-        ThirdSomething,
-        FourthAwakens,
-        FifthElement,
-        SixthSensor,
-        SeventhOfNine,
-        EightsAfterlife,
-        NinthAndLastLifeOfThisCat
+        None = 0,
+        FirstBlood = 1,
+        SecondMinute = 2,
+        ThirdSomething = 4,
+        FourthAwakens = 8,
+        FifthElement = 16,
+        SixthSensor = 32,
+        SeventhOfNine = 64,
+        EightsAfterlife = 128,
+        NinthAndLastLifeOfThisCat = 256
     }
 }
